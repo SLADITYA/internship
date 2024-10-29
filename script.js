f9208fa98264de751cf6b51e235430c98d053906
Aditya <22a91a05f9@aec.edu.in>
2024-10-29 07:01:13 +0530

    internship

---
+++ script.js
@@ -0,0 +1,47 @@
+const apiKey = "fbab96ca9c468702a4e5dc8eb46f50ec";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json();
+        console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        switch (data.weather[0].main) {
+            case "Clouds":
+                weatherIcon.src = "Images/clouds.png";
+                break;
+            case "Clear":
+                weatherIcon.src = "Images/clear.png";
+                break;
+            case "Rain":
+                weatherIcon.src = "Images/rain.png";
+                break;
+            case "Drizzle":
+                weatherIcon.src = "Images/drizzle.png";
+                break;
+            case "Mist":
+                weatherIcon.src = "Images/mist.png";
+                break;
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
